8b88524a93d1f6e45e87714c031ef61d6a5c4c36
GreenRefraction <axel.gronland@gmail.com>
2023-07-08 00:27:41 +0200

    slight change in reward

--- qtttgym/env.py
+++ qtttgym/env.py
@@ -3,6 +3,7 @@ from qtttgym import QEvalClassic
 from qtttgym import displayBoard
 
 import gymnasium as gym
+import numpy as np
 from gymnasium.spaces import Discrete, Tuple, Dict, Box
 from ray.rllib.utils.spaces.repeated import Repeated
 
@@ -19,7 +20,7 @@ class Env(gym.Env):
         self.observation_space = Dict({
             "q_states_p1" : Repeated(Tuple((Discrete(9), Discrete(9))), 5),
             "q_states_p2" : Repeated(Tuple((Discrete(9), Discrete(9))), 4),
-            "classical"   : Box(-1, 1, shape=(9,), dtype=int),
+            "classical"   : Box(-1, 1, shape=(9,), dtype=np.int32),
             "turn"        : Discrete(2),
         })
         self._gameboard = Board(QEvalClassic())
@@ -31,7 +32,7 @@ class Env(gym.Env):
         }
         
     def step(self, action: ActType, verbose=False) -> tuple[ObsType, RewType, bool, bool]:
-        cur_player = self.turn % 2
+        cur_player = self.turn() % 2
         try:
             squareFirst = action[0]
             squareSecond = action[1]
@@ -43,10 +44,13 @@ class Env(gym.Env):
         # unsure about including the binary value for which players turn it is
         # this information is given implicitly in the state vector, but I'm not sure
         obs = self._observation() #+ (cur_player,)
-        rew = self._reward()
-        terminated = any(map(lambda r: r != 0, rew)) or self.turn() > 8
+        
+        p1_round, p2_round = self._gameboard.check_win()
+        r = (-1 **cur_player) * float(p1_round > 0 or p2_round > 0)
+        # rew = self._reward()
+        terminated = (p1_round > 0 or p2_round > 0) or self.turn() > 8
         truncated = False
-        return obs, rew, terminated, truncated, {}
+        return obs, r, terminated, truncated, {}
 
     def reset(self, *, seed=None, options=None) -> ObsType:
         self.__init__()
@@ -72,7 +76,13 @@ class Env(gym.Env):
                     q_states_player2.append(move[:-1])
                 else:
                     q_states_player1.append(move[:-1])
-        return (q_states_player1, q_states_player2, classical_state)
+        out = {
+            "q_states_p1" : q_states_player1,
+            "q_states_p2" : q_states_player2,
+            "classical"   : classical_state,
+            "turn"        : self.turn()%2,
+        }
+        return out
 
     def _reward(self):
         """
@@ -98,4 +108,5 @@ class Env(gym.Env):
             p1_rew = self._reward_map["loss"]
             p2_rew = self._reward_map["win"]
 
-        return (p1_rew, p2_rew)
+        return p1_rew
+        # return (p1_rew, p2_rew)
