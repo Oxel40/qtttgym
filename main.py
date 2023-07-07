93c52f6360fdeb57b519ad7b52dab59b3faa1f62
GreenRefraction <axel.gronland@gmail.com>
2023-07-08 00:27:41 +0200

    slight change in reward

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import ray.rllib as rllib
 import ray.rllib.utils
 
 # board = qtttgym.Board(qtttgym.QEvalClassic())
-env = qtttgym.Env()
+# env = qtttgym.Env()
 ray.rllib.utils.check_env(qtttgym.Env())
